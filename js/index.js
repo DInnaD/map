1ecb8c080d1a2b328cf616f52ed621b3b5be428d
DInnaD <DInnaD@users.noreply.github.com>
2020-02-01 20:55:25 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ var GM = {
 
     initMap: function () {
         var coordinates = {lat: 49.580158, lng: 34.545727},
-            popupContent = '<p class="content" style="text-color:black;"><h6>Мерщій на концерт, поспішай, зупинити біля Симфонічного Оркестру вимагай!</h6></p>'/*this.$popupContent.html()*/,
+            popupContent = '<p class="content" style="text-color:black!important;"><h6>Мерщій на концерт, поспішай, зупинити біля Симфонічного Оркестру вимагай!</h6></p>'/*this.$popupContent.html()*/,
             markerImage = 'images/marker.png',
             zoom = 15,
 
